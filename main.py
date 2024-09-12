88c2e0b7d33790de1f6f139831f925393d716343
Filipp Voronov <fivoronov@gmail.com>
2024-09-12 18:33:17 +0300

    Calc total for tourism

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 spb_price = 50_000
 msc_price = 80_000
 ekb_price = 40_000
+
+city = input("Введите город: ")
+count = int(input("Введите количество туристов: "))
+
+if city == "spb":
+    total = spb_price * count
+elif city == "msc":
+    total = msc_price * count
+elif city == "ekb":
+    total = ekb_price * count
+else:
+    print("Такого города нет")
+    exit()
+
+print("Итоговый бюджет: " + str(total))
